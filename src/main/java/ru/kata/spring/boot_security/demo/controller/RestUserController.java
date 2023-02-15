297e54872717afb2fb4aca02831209cbb18ca184
IZMarshmelo <zd16@bk.ru>
2023-02-15 11:53:46 +0300

    Last version

--- src/main/java/ru/kata/spring/boot_security/demo/controller/RestUserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/RestUserController.java
@@ -3,19 +3,29 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
+import ru.kata.spring.boot_security.demo.util.UserErrorResponse;
+import ru.kata.spring.boot_security.demo.util.UserNotCreatedException;
 
-import java.security.Principal;
-import java.util.Optional;
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("api/user")
+@RequestMapping("/api/admin")
 public class RestUserController {
 
     private final UserService userService;
@@ -25,10 +35,54 @@ public class RestUserController {
         this.userService = userService;
     }
 
-    @GetMapping
-    public ResponseEntity<Optional<User>> getUser(Principal principal) {
-        Optional<User> user = userService.findByEmail(principal.getName());
-        return new ResponseEntity<>(user, HttpStatus.OK);
+    @GetMapping()
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> response = userService.getAllUsers();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
+        User response = userService.getUserById(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<HttpStatus> createUser (@RequestBody @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            StringBuilder errorMsg = new StringBuilder();
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for (FieldError error : errors) {
+                errorMsg.append(error.getField())
+                        .append(" - ").append(error.getDefaultMessage())
+                        .append(";");
+            }
+
+            throw new UserNotCreatedException(errorMsg.toString());
+        }
+
+        userService.addUser(user);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PutMapping()
+    public ResponseEntity<HttpStatus> editUser (@RequestBody User user, @PathVariable("id") Long id) {
+        userService.editUser(user, id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") Long id) {
+        userService.removeUserById(id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<UserErrorResponse> handlerException(UserNotCreatedException e) {
+        UserErrorResponse response = new UserErrorResponse(
+                e.getMessage(),
+                System.currentTimeMillis());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
 }
